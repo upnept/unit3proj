f890754fbba6444b2ce17717169f03470c4e966a
= <chris.mejia@southeasttech.edu>
2026-05-02 18:31:20 -0500

    state for light/dark

--- unit3project/src/App.tsx
+++ unit3project/src/App.tsx
@@ -4,7 +4,8 @@ import './App.css'
 function App() {
   let saved = JSON.parse(localStorage.getItem("items") || "[]");
 
-  let [items, setItems] = useState<string[]>(saved);
+  let [items, setItems] = useState(saved);
+  let [lightingMode, setLightingMode] = useState(false);
 
   return (
     <>
