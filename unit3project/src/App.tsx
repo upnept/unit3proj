dc729e8ab36ed59b55c6c6341df2e25742d1f552
= <chris.mejia@southeasttech.edu>
2026-05-02 18:26:37 -0500

    msginput transfer

--- unit3project/src/App.tsx
+++ unit3project/src/App.tsx
@@ -2,30 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-let saved = JSON.parse(localStorage.getItem("items") || "[]");
+  let saved = JSON.parse(localStorage.getItem("items") || "[]");
 
   let [items, setItems] = useState<string[]>(saved);
-  let [input, setInput] = useState("");
-
-  function addItem() {
-    let updated = items.concat(input);
-
-    setItems(updated);
-    localStorage.setItem("items", JSON.stringify(updated));
-
-    setInput("");
-  }
 
   return (
     <>
-      <form onSubmit={addItem}>
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="add item"
-        />
-      </form>
-
       <div>
         {items.map((item) => (
           <p>{item}</p>
--- unit3project/src/MessageInput.tsx
+++ unit3project/src/MessageInput.tsx
@@ -1,5 +1,24 @@
-export function MessageInput() {
+import { useState } from 'react'
+
+export function MessageInput(items, setItems) {
+    let [input, setInput] = useState("");
+
+    function addItem() {
+        let updated = items.concat(input);
+
+        setItems(updated);
+        localStorage.setItem("items", JSON.stringify(updated));
+
+        setInput("");
+    }
+
     return <>
-    
+        <form onSubmit={addItem}>
+            <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Add Message"
+            />
+        </form>
     </>
 }
